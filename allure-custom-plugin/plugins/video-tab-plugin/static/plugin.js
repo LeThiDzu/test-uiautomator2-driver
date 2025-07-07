f466473f54b137d8cba2b70901833e2b166f3752
Tran Huu Hoan <hoanth.bhsoft@gmail.com>
2025-07-07 13:40:44 +0700

    Update video path

--- allure-custom-plugin/plugins/video-tab-plugin/static/plugin.js
+++ allure-custom-plugin/plugins/video-tab-plugin/static/plugin.js
@@ -14,8 +14,7 @@
     onRender: function () {
       const testName = this.model.get('name');
       const videoName = testName.replace(/\s/g, "_")
-      const uuid = this.model.get('uuid');
-      const test_status = this.model.get('status')
+      const testStatus = this.model.get('status');
       const basePath = window.location.origin + window.location.pathname.split('/').slice(0, -1).join('/');
       const videoUrl = `${basePath}/Videos/${videoName}.mp4`;
       this.$('#test-name').append(`<h2>${testName}</h2>`);
@@ -29,4 +28,24 @@
     }
   })
   allure.api.addTestResultTab('test-video', 'Test video Tab', TestDetailNewTabView)
+
+  let VideoTabView = Backbone.Marionette.View.extend({
+    regions: {
+      test_result: '.test-result__content'
+    },
+    template: function () {
+      return `
+          <h2>Test Videos</h2>
+          <div ng-repeat="test in testVideos">
+              <h4>{{test.name}}</h4>
+              <video width="640" height="360" controls ng-if="test.videoUrl">
+              <source ng-src="{{trustSrc(test.videoUrl)}}" type="video/mp4">
+              Your browser does not support the video tag.
+              </video>
+              <p ng-if="!test.videoUrl">No video available.</p>
+              <hr>
+          </div>
+      `
+    }
+  })
 })()
